8fa50152eabdfa817f667a13715a5d276b2bf638
KangSeongCheol <80453145+2020311920@users.noreply.github.com>
2025-05-17 17:07:22 +0900

    [Silver II] Title: 스택 수열, Time: 16 ms, Memory: 2552 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.cc
+++ 백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -3,43 +3,42 @@
 using namespace std;
 
 int n;
+stack<int> s;
 int cnt=1;
-vector<char> v;
+vector<char> ans;
 
 int main(){
-    //ios::sync_with_stdio(0);
-    //cin.tie(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    cin>>n;
-    stack<int> s;
     s.push(0);
+    cin>>n;
+
     for(int i=1;i<=n;i++){
         int num;
         cin>>num;
+        while(s.top()!=num && cnt<=n+1){
+            //사용자 입력한 수가 s.top에 존재하는가?
+            //s.top을 비어있을 때는 할 수 없으니까, 미리 0을 넣어둔다.
 
-        while(num!=s.top() && cnt<=n){
             s.push(cnt);
-            //cout<<cnt<<"를 푸쉬함\n";
+            ans.push_back('+');
             cnt++;
-            v.push_back('+');
         }
         
-        if(cnt>n+1){
-            break;
-        }
-        if(num==s.top()){
-            //cout<<s.top()<<"이랑 "<<num<<"이 같아서 pop\n";
+        //이제는 찾았겠지 아니면 이미 다 n까지 넣어봤거나
+        if(s.top()==num){
             s.pop();
-            v.push_back('-');
+            ans.push_back('-');
         }
+
         else{
+            //이 말은 그냥 못찾은거니까 불가한거죠
             break;
         }
-
     }
 
-    if(s.top()==0){
-        for(auto i : v) cout<<i<<"\n";
-    }
-    else cout<<"NO";
+    //for문 다 돌았어.. 근데 만약 결국 불가했다면? 스택에 원소가 남아있겠지
+    if(s.top()!=0) cout<<"NO";
+    else for(auto i : ans) cout<< i<<"\n";
 }
