79a7f650e668283cec0f1e154e60abaff3804557
KangSeongCheol <80453145+2020311920@users.noreply.github.com>
2026-02-11 22:21:35 +0900

    [Gold I] Title: 그래프 트리 분할, Time: 64 ms, Memory: 12620 KB -BaekjoonHub

--- 백준/Gold/22954. 그래프 트리 분할/그래프 트리 분할.cc
+++ 백준/Gold/22954. 그래프 트리 분할/그래프 트리 분할.cc
@@ -135,8 +135,9 @@ void input(){
 }
 
 int main(){
-    // cin.tie(0);
-    // ios::sync_with_stdio(0);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
 
     input();
     solution();
