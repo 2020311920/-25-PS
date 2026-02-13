6c7f19842760ed36b7181b05980916767afdbf5b
KangSeongCheol <80453145+2020311920@users.noreply.github.com>
2026-02-13 17:35:30 +0900

    [Gold III] Title: 파티, Time: 0 ms, Memory: 2344 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.cc
+++ 백준/Gold/1238. 파티/파티.cc
@@ -8,6 +8,8 @@ struct node{
     vector<pair<int, int>> to; 
 };
 node nodes[1001]; //nodes[a].to[b].first, second -> a가 first로 가는데 second만큼의 비용이 있다.
+//역그래프
+node rnodes[1001];
 
 void input(){
     cin>>n>>m>>goal;
@@ -15,75 +17,52 @@ void input(){
         int x, y, c;
         cin>>x>>y>>c;
         nodes[x].to.push_back({y,c});
+        rnodes[y].to.push_back({x,c});
     }
 }
 
-int getCost(int start, int goal){
-    vector<int> dist;
-    dist.assign(n+1, INTMAX);
+int toGoal[1001];
+int fromGoal[1001];
+
+void runDijk(node nodes[], int distArr[]){
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    //pq는 {cost, 현재 위치};
-    int totalCost = 0;
-    pq.push({0, start});
-    dist[start]=0;
-    while(!pq.empty()){
-        int s = pq.top().second;
-        int c = pq.top().first; pq.pop();
-        if(dist[s]!=c) continue;
-        if(s==goal){
-            totalCost += c;
-            break;
-        }
-
-        for(auto i : nodes[s].to){
-            //s는 i.first로 갈 수 있고, 그 비용은 i.second
-            if(dist[i.first]>c+i.second){
-                dist[i.first] = c+i.second;
-                pq.push({c+i.second, i.first});
-            }
-            
-        }
+    //{cost, position}
+    for(int i=1;i<=n;i++){
+        distArr[i]=INTMAX;
     }
 
-    while(!pq.empty()) pq.pop(); //pq를 비울거에요
-    dist.clear();
-    dist.assign(n+1, INTMAX);
-    //이제 도착지에 도착을 했기 때문에 goal에서 start로 갈겁니다.
-    pq.push({0, goal});
-    dist[goal]=0;
+    pq.push({0,goal});
+    distArr[goal] = 0;
     while(!pq.empty()){
         int s = pq.top().second;
-        int c = pq.top().first; pq.pop();
-        if(dist[s]!=c) continue;
-        if(s == start){
-            totalCost += c;
-            break;
+        int c = pq.top().first;
+        pq.pop();
+
+        if(distArr[s]!=c){
+            continue;
         }
 
         for(auto i : nodes[s].to){
-            if(dist[i.first]>c+i.second){
-                dist[i.first] = c+i.second;
+            if(distArr[i.first]>c+i.second){
+                distArr[i.first] = c+i.second;
                 pq.push({c+i.second, i.first});
             }
         }
     }
-
-    return totalCost;
 }
 
 void solution(){
-    int maxTotalCost=0;
-    for(int i=1;i<=n;i++){
-        if(i==goal) continue;
-        int start = i;
-        int totalCost=0;
+    runDijk(nodes, fromGoal);
+    //그래프 역방향으로
+    runDijk(rnodes, toGoal);
 
-        //start에서 goal로 가고, 다시 goal에서 start로 가는 총 cost
-        int tempCost = getCost(start, goal);
-        if(tempCost>maxTotalCost) maxTotalCost = tempCost;
+    int maxCost = 0;
+    for(int i=1;i<=n;i++){
+        int sumV = fromGoal[i]+toGoal[i];
+        if(sumV>maxCost) maxCost = sumV;
     }
 
-    cout<<maxTotalCost;
+    cout<<maxCost;
 }
 
 int main(){
