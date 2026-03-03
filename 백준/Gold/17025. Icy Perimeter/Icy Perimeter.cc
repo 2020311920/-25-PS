43eb5d601458e5d433d98e4391ef6f7590fb7b64
KangSeongCheol <80453145+2020311920@users.noreply.github.com>
2026-03-03 14:05:01 +0900

    [Gold V] Title: Icy Perimeter, Time: 56 ms, Memory: 9852 KB -BaekjoonHub

--- 백준/Gold/17025. Icy Perimeter/Icy Perimeter.cc
+++ 백준/Gold/17025. Icy Perimeter/Icy Perimeter.cc
@@ -3,8 +3,8 @@
 using namespace std;
 
 int n;
-vector<pair<int, int>> iceCream;
-char field[1001][1001];
+//vector<pair<int, int>> iceCream;
+int field[1001][1001];
 
 int visited[1001][1001];
 
@@ -15,10 +15,10 @@ void input(){
             char ch;
             cin>>ch;
             if(ch=='#'){
-                field[i][j]=0;
-                iceCream.push_back({i,j});
+                field[i][j]=1;
+                //iceCream.push_back({i,j});
             }
-            if(ch=='.') field[i][j]=1;
+            if(ch=='.') field[i][j]=2;
         }
     }
 }
@@ -42,7 +42,7 @@ pair<int,int> bfs(int startX, int startY, int blockInd){
             int ny = sy+dirY[i];
 
             if(nx>=1 && nx<=n && ny>=1 && ny<=n){
-                if(field[nx][ny]==0){
+                if(field[nx][ny]==1){
                     if(visited[nx][ny]==0){
                         visited[nx][ny]=blockInd; cnt++;
                         q.push({nx,ny});
@@ -65,27 +65,27 @@ pair<int,int> bfs(int startX, int startY, int blockInd){
 
 void solution(){
     //iceCream의 모든 좌표들을 시작점으로 해서 덩어리를 구하는거지 ㅇㅇ
-    int iceCreamNum = iceCream.size();
+    //int iceCreamNum = iceCream.size();
     int blockInd=1;
     int maxBlockSize = 0;
     int minBorderLine = 2100000000;
-    for(int i=0;i<iceCreamNum;i++){
-        int x = iceCream[i].first; int y = iceCream[i].second;
-
-        if(visited[x][y]==0){
-            //아직 안 들른 곳이어야 덩어리의 첫 좌표로서 시작 가능인거임
-            visited[x][y]=blockInd;
-            auto blockInfo = bfs(x,y, blockInd);
-            if(maxBlockSize<blockInfo.second){
-                //최신화
-                //while(!ansBlock.empty()) ansBlock.pop();
-                maxBlockSize = blockInfo.second;
-                minBorderLine = blockInfo.first;
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(visited[i][j]==0 && field[i][j]==1){
+                //아직 안 들른 곳이어야 덩어리의 첫 좌표로서 시작 가능인거임
+                visited[i][j]=blockInd;
+                auto blockInfo = bfs(i,j, blockInd);
+                if(maxBlockSize<blockInfo.second){
+                    //최신화
+                    //while(!ansBlock.empty()) ansBlock.pop();
+                    maxBlockSize = blockInfo.second;
+                    minBorderLine = blockInfo.first;
+                }
+                else if(maxBlockSize == blockInfo.second){
+                    minBorderLine = min(minBorderLine, blockInfo.first);
+                }
+                blockInd++;
             }
-            else if(maxBlockSize == blockInfo.second){
-                minBorderLine = min(minBorderLine, blockInfo.first);
-            }
-            blockInd++;
         }
     }
     //visited에 blockInd가 여러개로 막 표시가 됐을 것이야
