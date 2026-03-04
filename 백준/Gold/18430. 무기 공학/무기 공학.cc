2ffb7a89a68bc66a320efccb6198dcb594003a3c
KangSeongCheol <80453145+2020311920@users.noreply.github.com>
2026-03-04 10:36:05 +0900

    [Gold IV] Title: 무기 공학, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/18430. 무기 공학/무기 공학.cc
+++ 백준/Gold/18430. 무기 공학/무기 공학.cc
@@ -40,48 +40,92 @@ bool isPossible(int a, int b){
     return false;
 }
 
-void dfs(int x, int y, int cnt, int sum){
-    //cnt라 함은 현재 사용한 칸의 개수라고 하자고
-    if(n*m-cnt<3){
+// void dfs(int x, int y, int cnt, int sum){
+//     //cnt라 함은 현재 사용한 칸의 개수라고 하자고
+//     if(n*m-cnt<3){
+//         maxSum = max(maxSum, sum);
+//         return;
+//     }
+
+//     for(int i=x;i<=n;i++){
+//         for(int j=1;j<=m;j++){
+//             if(visited[i][j]==0){
+//                 //i,j로 하여금 중심점이 되게 한느거야
+                
+//                 for(int a=0;a<4;a++){
+//                     //모양 설정
+//                     //a라는 모양을 택했으니 나머지 좌표들 설정
+//                     int x1 = i+dirX[a][0];
+//                     int x2 = i+dirX[a][1];
+
+//                     int y1 = j+dirY[a][0];
+//                     int y2 = j+dirY[a][1];
+
+//                     //총 세점으로 이동이 가능한지 판단
+//                     if(isPossible(x1,y1) && isPossible(x2,y2)){
+//                         //실제로 체크하고 해당 세계관 열어
+//                         visited[i][j]=1;
+//                         visited[x1][y1]=1;
+//                         visited[x2][y2]=1;
+                        
+//                         dfs(i,j,cnt+3, sum+(field[i][j]*2 + field[x1][y1] + field[x2][y2]));
+
+//                         visited[i][j]=0;
+//                         visited[x1][y1]=0;
+//                         visited[x2][y2]=0;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+
+//     //여기로 온다는 거는 ㄹㅇ로 칸이 있기는 한데 더이상 쪼갤 수가 없는 경우 아닌가
+//     maxSum = max(maxSum, sum);
+//     return;
+// }
+
+void dfs(int r, int c, int sum) {
+    // 1. 열(c)이 끝을 벗어나면 다음 줄(r+1)의 첫 번째 칸으로 이동
+    if (c > m) {
+        r++;
+        c = 1;
+    }
+
+    // 2. 행(r)이 끝을 벗어나면 탐색 종료 (기저 조건)
+    if (r > n) {
         maxSum = max(maxSum, sum);
         return;
     }
 
-    for(int i=x;i<=n;i++){
-        for(int j=1;j<=m;j++){
-            if(visited[i][j]==0){
-                //i,j로 하여금 중심점이 되게 한느거야
-                
-                for(int a=0;a<4;a++){
-                    //모양 설정
-                    //a라는 모양을 택했으니 나머지 좌표들 설정
-                    int x1 = i+dirX[a][0];
-                    int x2 = i+dirX[a][1];
+    // [선택지 1] 이 칸을 중심으로는 부메랑을 안 만들고 그냥 다음 칸(c+1)으로 넘어가기
+    // for문으로 다음 칸을 찾는 행위를 이 재귀 호출이 대신해 줍니다.
+    dfs(r, c + 1, sum);
 
-                    int y1 = j+dirY[a][0];
-                    int y2 = j+dirY[a][1];
+    // [선택지 2] 이 칸이 아직 방문 안 된 상태라면, 여기서 4가지 모양 시도해보기
+    if (visited[r][c] == 0) {
+        for (int a = 0; a < 4; a++) {
+            int x1 = r + dirX[a][0];
+            int y1 = c + dirY[a][0];
+            int x2 = r + dirX[a][1];
+            int y2 = c + dirY[a][1];
 
-                    //총 세점으로 이동이 가능한지 판단
-                    if(isPossible(x1,y1) && isPossible(x2,y2)){
-                        //실제로 체크하고 해당 세계관 열어
-                        visited[i][j]=1;
-                        visited[x1][y1]=1;
-                        visited[x2][y2]=1;
-                        
-                        dfs(i,j,cnt+3, sum+(field[i][j]*2 + field[x1][y1] + field[x2][y2]));
+            if (isPossible(x1, y1) && isPossible(x2, y2)) {
+                // 세계관 열기 (방문 처리)
+                visited[r][c] = 1;
+                visited[x1][y1] = 1;
+                visited[x2][y2] = 1;
 
-                        visited[i][j]=0;
-                        visited[x1][y1]=0;
-                        visited[x2][y2]=0;
-                    }
-                }
+                // 부메랑을 만들었으니 다음 칸으로 넘어가며 sum 증가
+                int currentSum = field[r][c] * 2 + field[x1][y1] + field[x2][y2];
+                dfs(r, c + 1, sum + currentSum);
+
+                // 세계관 닫기 (방문 해제)
+                visited[r][c] = 0;
+                visited[x1][y1] = 0;
+                visited[x2][y2] = 0;
             }
         }
     }
-
-    //여기로 온다는 거는 ㄹㅇ로 칸이 있기는 한데 더이상 쪼갤 수가 없는 경우 아닌가
-    maxSum = max(maxSum, sum);
-    return;
 }
 
 void solution(){
@@ -92,8 +136,8 @@ void solution(){
 
     //이게 뒤에거를 만들고, 다시 앞에서부터 검색하면서 놓으면 중복이 되니까, 오른아래로만 검색할 수 있게 해야겠다.
 
-    dfs(0,0,0,0);
-
+    //dfs(0,0,0,0);
+    dfs(1,1,0);
     cout<<maxSum;
 }
 
