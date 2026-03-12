a8b25240885db27bf9ee170a68ec1fe24ac79608
KangSeongCheol <80453145+2020311920@users.noreply.github.com>
2026-03-12 19:00:49 +0900

    [Gold IV] Title: 인구 이동, Time: 420 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/16234. 인구 이동/인구 이동.cc
+++ 백준/Gold/16234. 인구 이동/인구 이동.cc
@@ -2,11 +2,9 @@
 
 using namespace std;
 
-int n, l, r;
+int n; int l,r;
 int field[51][51];
-int dirX[4]={0,1,0,-1};
-int dirY[4]={1,0,-1,0};
-bool trigger;
+
 void input(){
     cin>>n>>l>>r;
     for(int i=1;i<=n;i++){
@@ -16,69 +14,76 @@ void input(){
     }
 }
 
-void bfs(){
-    trigger = false;//초기화
-    int visited[51][51];
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            visited[i][j]=0;
-        }
-    }
+int visited[51][51];
 
+int dirX[4]={0,1,0,-1};
+int dirY[4]={1,0,-1,0};
+
+bool bfs(){
+    bool returnV=false;
+    //시작점을 탐색하고, 그 시작점부터 bfs돌려
+    int sectorN = 0;
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
             if(visited[i][j]==0){
-                queue<pair<int, int>> q;
+                //i,j가 시작점이 돼
+                int sectorSize = 1;
+                int sectorSum = field[i][j];
+                sectorN++;
+                //sectorN번째 구역 탐색 시작
+                visited[i][j]=sectorN;
                 vector<pair<int, int>> v;
-                q.push({i,j});
-                visited[i][j]=1;
                 v.push_back({i,j});
-                int tempSum=field[i][j];
-                while(!q.empty()){
-                    auto start = q.front(); q.pop();
+                queue<pair<int, int>> q;
+                q.push({i,j});
 
-                    int sx = start.first; int sy = start.second;
-                    for(int k=0;k<=3;k++){
+                while(!q.empty()){
+                    int sx = q.front().first; int sy = q.front().second;
+                    q.pop();
+                    
+                    for(int k=0;k<4;k++){
                         int nx = sx+dirX[k];
                         int ny = sy+dirY[k];
+
                         if(nx>=1 && nx<=n && ny>=1 && ny<=n){
                             if(visited[nx][ny]==0){
-                                if(abs(field[nx][ny]-field[sx][sy])>=l && abs(field[nx][ny]-field[sx][sy])<=r){
-                                    //범위 안이고, 방문 안했고, 연합조건도 맞으니
-                                    visited[nx][ny]=1;
-                                    tempSum+=field[nx][ny];
-                                    q.push({nx,ny});
+                                if(abs(field[sx][sy]-field[nx][ny])>=l && abs(field[sx][sy]-field[nx][ny])<=r){
+                                    //이럼 국경선이 열린다, bfs로 접근이 가능하다
+                                    visited[nx][ny]=sectorN;
                                     v.push_back({nx,ny});
-                                }   
+                                    q.push({nx, ny});
+                                    sectorSize++;
+                                    sectorSum+=field[nx][ny];
+                                }
                             }
                         }
                     }
                 }
-                //구역확장 완료
-                int sectorSize = v.size();
-                //구역의 크기가 2이상이면 인구이동이 일어남
-                //한번이라도 일어나면 하루는 지난다.
-                if(sectorSize>=2) trigger = true;
-                tempSum /= sectorSize;
 
-                for(auto coord:v){
-                    field[coord.first][coord.second] = tempSum;
+                if(sectorSize>1){
+                    returnV = true;
+                    //국경 열린게 있다는거라서
+                    int average = sectorSum/sectorSize;
+                    for(auto x : v){
+                        field[x.first][x.second] = average;
+                    }
                 }
+
             }
         }
     }
+
+    return returnV;
 }
 
 void solution(){
     int days=0;
     while(1){
-        bfs();
-        if(trigger){
-            days++;
-        }
-        else break;
+        memset(visited, 0, sizeof(visited));
+        bool returnV = bfs(); //구역들 열심히 나누면서 알아서 바로바로 업데이트하도록 하자
+        if(!returnV) break;
+        days++;
     }
-
     cout<<days;
 }
 
